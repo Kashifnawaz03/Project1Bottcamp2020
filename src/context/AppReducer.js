b7aa474018aefd495cfe69286ad545d50920497d
kashifnawaz <kashi030@hotmail.com>
2021-04-17 03:18:40 +0500

    Trying again to fix

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,5 @@
 
-export default (state, action) => {
+const fn = (state, action) => {
     switch(action.type) {
         case 'DELETE_TRANSACTION':
             return{
@@ -16,3 +16,4 @@ export default (state, action) => {
             return state;
     }
 }
+export default fn;
